bbd8ef86b72e55de7fd5c4e4f5f90a99c6468e01
tolgamorf <tolga@hetge.com>
2022-01-19 02:38:10 +0300

    Added tRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { trpc } from "@/utils/trpc"
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div className="text-2xl text-red-500">Hello World</div>
+  const {data, isLoading } = trpc.useQuery(["hello", {text: "Theo"}])
+
+  if (isLoading) return <div>Loading...</div>
+
+  if (data) return <div>{data.greeting}</div>
+
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+      <div className="w-16 h-16 bg-red-200" />
+      <div className="p-8">Vs</div>
+      <div className="w-16 h-16 bg-red-200" />
+      </div>
+    </div>
+)
 }
 
 export default Home
